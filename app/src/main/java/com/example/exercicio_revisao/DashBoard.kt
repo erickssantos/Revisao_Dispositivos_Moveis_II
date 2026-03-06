38235bcb636d6df0f7509232ade3a93b7c9ddffc
erickssantos <erickmiguelbsantos@gmail.com>
2026-03-06 15:37:35 -0300

    Versão final

--- app/src/main/java/com/example/exercicio_revisao/DashBoard.kt
+++ app/src/main/java/com/example/exercicio_revisao/DashBoard.kt
@@ -5,12 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.exercicio_revisao.databinding.ActivityDashBoardBinding
 
 class DashBoard : AppCompatActivity() {
+
+    private lateinit var binding:  ActivityDashBoardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dash_board)
+        binding = ActivityDashBoardBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val loginUsuario = intent.getStringExtra("LOGIN_USUARIO") ?: "Desconhecido"
+        binding.txtusuario.text="Bem-vindo, $loginUsuario"
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/exercicio_revisao/MainActivity.kt
+++ app/src/main/java/com/example/exercicio_revisao/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.exercicio_revisao
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.exercicio_revisao.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val login = binding.loginInput.text.toString().trim()
+            val senha = binding.senhaInput.text.toString().trim()
+
+            if (login == "admin" && senha =="1234") {
+                val intent = Intent(this, DashBoard::class.java)
+                intent.putExtra("LOGIN_USUARIO", login)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this, "Login ou Senha incorretos", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
 }
